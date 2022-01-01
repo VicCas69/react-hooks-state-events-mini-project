73b4e8250dfb88ce14aa14718d06ee15e1f719dc
victor castro <victor_castro_01@hotmail.com>
2021-12-31 23:42:42 -0500

    Done.

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
+import { CATEGORIES, TASKS } from "../data";
 
-function CategoryFilter() {
+function CategoryFilter({ categories }) {
+
+  //const [category, setCategory] = useState(categories);
+  //const [selected, setSelected] = useState('selected')
+
+  const handleFilter = (e) => {
+    e.target.className = 'selected' 
+    const selected = categories.map((name) => name) //=== e.target.innerText)
+    
+    //const finalSelected = selected.map((each)=> each.className !== 'selected')
+    //console.log(finalSelected)    
+    //console.log(selected)
+}
+
+
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map((category) => {
+        return <button key={category.index} onClick={handleFilter}>{category}</button>
+      })}
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Task from "./Task"
 
-function TaskList() {
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
+function TaskList({tasks}) {
+
+const [removeTask, setRemoveTask] = useState(tasks);
+
+function handleDelete(e){
+  const filteredList =  removeTask.filter((task)=>{
+    return task.text !== e.target.parentNode.children[1].innerText
+  })
+  //console.log(filteredList)
+  //console.log(tasks)
+  setRemoveTask(filteredList)
+}
+  return ( 
+  <div className="tasks">
+      <ul>
+      {removeTask.map((task) => {
+        //console.log(task);
+        //Do not forget the keyword return or replace curly braces with parenthesis
+          return <Task key={task.index} name={task.text} category={task.category} handleDelete={handleDelete}/>
+      })}
+
+      </ul>
     </div>
   );
 }
